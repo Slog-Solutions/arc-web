098ec1a5498eeee5bf081627aecf526648089b98
Cocogoat28 <gvansh282@gmail.com>
2026-07-11 13:27:03 +0530

    Match subpage layout background to navbar color to fix two-tone effect

--- src/components/layout/SubPageLayout.tsx
+++ src/components/layout/SubPageLayout.tsx
@@ -35,7 +35,7 @@ export default function SubPageLayout({
       transition={{ duration: 0.5 }}
       className="min-h-screen pb-32 relative overflow-hidden"
       style={{
-        background: '#0a0c08',
+        background: '#050806',
         paddingTop: '180px',
       }}
     >
